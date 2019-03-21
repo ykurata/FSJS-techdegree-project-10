1d04d5d58699384330ef5fcb1258f6271ae4b2c4
ykuruta <yasukokurata@gmail.com>
2019-03-21 18:52:21 -0400

    connect to rest api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  // Initialize the state
+  constructor(props) {
+    super();
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount(){
+    this.getCourses();
+  }
+
+  // getCourses = () => {
+  //   fetch('/api/courses')
+  //   .then(res => res.json())
+  //   .then(listData => {
+  //     this.setState({ list: listData });
+  //   })
+  // }
+
+  getCourses = () => {
+    axios.get('/api/courses')
+      .then(response => {
+        this.setState({
+          list: response.data
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
+
   render() {
+    const { list } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>List of Items</h1>
+        {/* Check to see if any items are found*/}
+        {list.length ? (
+          <div>
+            {/* Render the list of items */}
+            {list.map((item, id) => {
+              return(
+                <div key={id}>
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>
+            <h2>No List Items Found</h2>
+          </div>
+        )
+      }
       </div>
     );
   }
